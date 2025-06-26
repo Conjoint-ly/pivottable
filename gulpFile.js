08c6c7305ca4c4cae6a1e74ca4ad16b2b7263886
Viktor Dergach <bruianio@gmail.com>
2025-06-26 17:05:07 +0000

    chore: update devDependencies and add new scripts in package.json
    - Upgraded gulp and related packages to their latest versions.
    - Replaced gulp-util with fancy-log and added gulp-clean-css.
    - Added new npm scripts for build, development, and versioning tasks.
    - Removed deprecated or outdated dependencies.

--- gulpFile.js
+++ gulpFile.js
@@ -1,48 +1,49 @@
-var gulp = require('gulp'),
-    git = require('gulp-git'),
-    bump = require('gulp-bump'),
-    filter = require('gulp-filter'),
-    tag_version = require('gulp-tag-version'),
-    runSequence = require('run-sequence').use(gulp),
-    spawn = require('child_process').spawn,
-    coffee = require('gulp-coffee'),
-    gutil = require('gulp-util'),
-    uglify = require("gulp-uglify"),
-    rename = require('gulp-rename'),
-    sourcemaps = require('gulp-sourcemaps'),
-    concat = require('gulp-concat'),
-    minifyCSS = require('gulp-minify-css'),
-    serve = require('gulp-serve');
+const gulp = require('gulp');
+const { series, parallel } = require('gulp');
+const git = require('gulp-git');
+const bump = require('gulp-bump');
+const filter = require('gulp-filter');
+const tag_version = require('gulp-tag-version');
+const spawn = require('child_process').spawn;
+const coffee = require('gulp-coffee');
+const uglify = require("gulp-uglify");
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const serve = require('gulp-serve');
+const log = require('fancy-log');
 
-gulp.task('makeCss', function() {
-    gulp.src('./dist/pivot.css')
-        .pipe(minifyCSS())
+function makeCss() {
+    return gulp.src('./dist/pivot.css')
+        .pipe(cleanCSS())
         .pipe(concat('pivot.min.css'))//trick to output to new file
-        .pipe(gulp.dest('./dist/'))
-});
+        .pipe(gulp.dest('./dist/'));
+}
 
-
-gulp.task('makeJs', function() {
-
-    gulp.src(['./src/*.coffee', './locales/*.coffee', './tests/*.coffee'])
+function makeJs() {
+    return gulp.src(['./src/*.coffee', './locales/*.coffee', './tests/*.coffee'])
         //compile to js (and create map files)
         .pipe(sourcemaps.init())
-        .pipe(coffee()).on('error', gutil.log)
+        .pipe(coffee()).on('error', log)
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist'))
+        .pipe(gulp.dest('./dist'));
+}
 
+function makeJsMin() {
+    return gulp.src(['./src/*.coffee', './locales/*.coffee', './tests/*.coffee'])
+        //compile to js (and create map files)
+        .pipe(sourcemaps.init())
+        .pipe(coffee()).on('error', log)
         //minify js files as well
-        .pipe(filter('*.js'))//filter, to avoid doing this processing on the map files generated above
-         .pipe(rename({
+        .pipe(rename({
             suffix: '.min'
         }))
         .pipe(sourcemaps.init({loadMaps: true}))//load the source maps generated in the first step
         .pipe(uglify())
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./dist'));
-});
-
-
+}
 
 function inc(importance) {
     // get all the files to bump version in
@@ -57,42 +58,56 @@ function inc(importance) {
 //   spawn('npm', ['publish'], { stdio: 'inherit' }).on('close', done);
 // });
 
-gulp.task('push', function (done) {
-  git.push('origin', 'master', {args: '--tags'}, function (err) {
-    if (err) throw err;
-  });
-});
+function push(done) {
+    git.push('origin', 'master', {args: '--tags'}, function (err) {
+        if (err) throw err;
+        done();
+    });
+}
 
-
-gulp.task('tag', function() {
+function tag() {
     return gulp.src(['./package.json', './bower.json', './pivottable.jquery.json'])
     .pipe(git.commit('version bump'))
     // read only one file to get the version number
     .pipe(filter('package.json'))
     .pipe(tag_version());
-});
+}
 
+function bumpPatch() { return inc('patch'); }
+function bumpMinor() { return inc('minor'); }
+function bumpMajor() { return inc('major'); }
 
-gulp.task('bumpPatch', function() { return inc('patch'); })
-gulp.task('bumpMinor', function() { return inc('minor'); })
-gulp.task('bumpMajor', function() { return inc('major'); })
+function patch() {
+    return series(bumpPatch, build, tag, push)();
+}
+function minor() {
+    return series(bumpMinor, build, tag, push)();
+}
+function major() {
+    return series(bumpMajor, build, tag, push)();
+}
 
-gulp.task('patch', function() {
-    runSequence('bumpPatch', 'default', 'tag', 'push');
-});
-gulp.task('minor', function() {
-    runSequence('bumpMinor', 'default', 'tag', 'push');
-});
-gulp.task('major', function() {
-    runSequence('bumpMajor', 'default', 'tag', 'push');
-});
+function watchFiles() {
+    gulp.watch(['./src/*.coffee', './locales/*.coffee', './tests/*.coffee'], parallel(makeJs, makeJsMin));
+    gulp.watch('./dist/pivot.css', makeCss);
+}
 
-gulp.task('serve', serve('.'));
+// Build task that combines makeJs and makeCss
+const build = parallel(makeJs, makeJsMin, makeCss);
 
-gulp.task('watch', function() {
-  gulp.watch(['./src/*.coffee', './locales/*.coffee', './tests/*.coffee'], ['makeJs']);
-  gulp.watch('./dist/pivot.css', ['makeCss']);
-});
-
-gulp.task('default', ['makeJs', 'makeCss']);
-
+// Export tasks
+exports.makeCss = makeCss;
+exports.makeJs = makeJs;
+exports.makeJsMin = makeJsMin;
+exports.bumpPatch = bumpPatch;
+exports.bumpMinor = bumpMinor;
+exports.bumpMajor = bumpMajor;
+exports.patch = patch;
+exports.minor = minor;
+exports.major = major;
+exports.push = push;
+exports.tag = tag;
+exports.serve = serve('.');
+exports.watch = watchFiles;
+exports.build = build;
+exports.default = build;
